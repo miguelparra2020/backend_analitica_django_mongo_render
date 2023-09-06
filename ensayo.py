30cacabb10f25af4686d737677bd6fa476123960
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-06 12:15:29 -0500

    Update

--- ensayo.py
+++ ensayo.py
@@ -1,50 +1,28 @@
 import requests
 import json
+def obtener_usuarios():
+    urlget = "https://us-east-1.aws.data.mongodb-api.com/app/data-jocyn/endpoint/data/v1/action/find"
 
-urlpost = "https://us-east-1.aws.data.mongodb-api.com/app/data-jocyn/endpoint/data/v1/action/insertOne"
-
-urlget = "https://us-east-1.aws.data.mongodb-api.com/app/data-jocyn/endpoint/data/v1/action/find"
-
-objectUsuario = {
-    "usuario": "NuevoUsuario1234",
-    "fecha_ingreso": "8/25/2023",
-    "hora_ingreso": "09:30:00",
-    "pais": "Colombia",
-    "ciudad": "Pereira",
-    "tiempo": "00:03:45",
-    "ruta": "/nueva-pagina",
-    "dispositivo": "Celular"
+    payload = {
+        "collection": "Usuarios",
+        "database": "Estadistica",
+        "dataSource": "estadistica",
+        "filter": {},
+        "limit": 1000000,
     }
 
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'LInXiCwRYyJIrl5I3Lsfc76iyQMSoxDx5FYhThbHexGwMepzzdYVmxETgp3hgXxw',
+        'Accept': 'application/ejson'
+    }
 
+    response = requests.post(urlget, headers=headers, json=payload)
 
-payload = json.dumps({
-    "collection": "Usuarios",
-    "document": objectUsuario,
-    "database": "Estadistica",
-    "dataSource": "estadistica",
-})
+    # Procesar la respuesta JSON
+    data = response.json()
+    return data
 
-payload2 = json.dumps({
-    "collection": "Usuarios",
-    "database": "Estadistica",
-    "dataSource": "estadistica",
-})
-
-headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Request-Headers': '*',
-    'api-key': 'LInXiCwRYyJIrl5I3Lsfc76iyQMSoxDx5FYhThbHexGwMepzzdYVmxETgp3hgXxw',
-    'Accept': 'application/ejson'
-}
-
-response = requests.request("POST", urlpost, headers=headers, data=payload)
-response2 = requests.request("POST", urlget, headers=headers, data=payload2)
-
-# Procesar la respuesta JSON
-data = json.loads(response.text)
-data2 = json.loads(response2.text)
-
-print(data)
-print(data2)
+print(obtener_usuarios())
